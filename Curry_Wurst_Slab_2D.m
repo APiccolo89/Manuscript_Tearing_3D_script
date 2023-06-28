a90ae72a3f76e89892aedeeb63d3f87e1d0672e7
APiccolo89 <85160533+APiccolo89@users.noreply.github.com>
2023-06-28 16:37:15 +0200

    Added an additional function
    One day I need to proper document it, and upgrade to a version that is usable, for now, I'm doing the little magician apprentice.

--- Curry_Wurst_Slab_2D.m
+++ Curry_Wurst_Slab_2D.m
@@ -16,7 +16,7 @@ Parallel_partition     = 'ProcessorPartitioning_8cpu_4.1.2.bin';
 T.R        = 40;   %curvature radius
 T.theta_c  = 30;   %curvature radius ingested continental crust
 T.theta_dc = 20;   % additional curvature to emulate passive margin (optional)
-T.theta    = 90;   % curvature slab
+T.theta    = 30;   % curvature slab
 T.tk_WZ    = 0;   % thickness of the weak zone
 T.L0       = 500;  % length of the slab from the bottom of the lithosphere
 T.D0       = 80;   % Thickness of the slab
@@ -27,9 +27,12 @@ T.r_WZ = [T.r(2), T.r(2)+T.tk_WZ];
 T.D_WZ = -100;
 T.Depth_C = -40;
 T.Length = 500;
-T.Type = 'Ribe_Mode'; % 'Ribe_Mode'
+T.Type = 'Mode_1'; % 'Ribe_Mode'
 T.CCS.phases = [13,1,2];
 T.CCS.Stratigraphy=[0,-12,-15,-30];
+T.Temperature = 'McKenzie';
+T.vl          = 1.0; 
+
 
 
 phases.Ph_Ar  = [0,10]  ;% Air
@@ -93,7 +96,7 @@ Oceanic_Plate.Type  = 'Ocean';
 Oceanic_Plate.x_lim = [0.0,0.0];
 Oceanic_Plate.Phases = [phases.Ph_OLt(1)];
 Oceanic_Plate.Stratigraphy = [0.0,-80.0];
-Oceanic_Plate.Age          = nan;
+Oceanic_Plate.Age          = 40;
 
 Oceanic_Plate.Trench        = 'Subduction';
 Oceanic_Plate.Trench_properties = T;
@@ -266,30 +269,121 @@ end
 
 
 function [Temp]= compute_temperature_profile(A,Temp,Type,Gen,Terranes,indx)
+
 T_tk = [0.0, Terranes.Stratigraphy(end)];
 k = Terranes.K./(Terranes.Cp.*3300);
 T_Age = Terranes.Age.*Terranes.secMyrsyear;
 T_P   = Gen.T_P;
 T_S   = Gen.T_S;
 Tsl   = Gen.AvTS;
+
+
 if Type == 1
+    Zpart = A.Zpart; 
     ind_z = find(A.Zpart<T_tk(1) & A.Zpart>=T_tk(2) & indx == 1);
     ind = A.Zpart< T_tk(2);
 
 else
     ind_z = A < T_tk(1) & A>=T_tk(2);
     ind = A < T_tk(2);
+    Zpart = A; 
+    
 
 end
 if isnan(T_Age)
     Temp(ind_z)=Tsl;
 else
 
-    erf_function = erf(A.Zpart(ind_z).*1000/2/(k*T_Age)^0.5);
+    erf_function = erf(Zpart(ind_z).*1000/2/(k*T_Age)^0.5);
     Temp(ind_z) = T_S - (T_P-T_S).*erf_function;
     Temp(ind) = T_P;
 
 end
+
+
+
+
+end
+
+function [Temp]= compute_temperature_profile_McKenzie(Layout,Temp,Type,Gen,Terranes,indx,A)
+
+T_tk = [0.0, Terranes.Stratigraphy(end)];
+k = Terranes.K;
+T_Age = Terranes.Age.*Terranes.secMyrsyear;
+T_P   = Gen.T_P;
+T_S   = Gen.T_S;
+Tsl   = Gen.AvTS;
+vl    = 10; 
+vl    = vl./1e2./365.25./24./60./60; 
+Re    = (3300*Terranes.Cp.*vl*Terranes.Trench_properties.D0.*1000)./2./k;
+% Compute the lenght of the slab starting from the trench
+%1) Prepare the vector
+z     = A.Zpart(:);
+x     = A.Xpart(:);
+%2) Select index that belongs to slab 
+d     = Layout(:);                    %layout filled with d
+slab  = abs(d)>=0 & abs(d)<=Terranes.Trench_properties.D0; % index layout
+l     = d.*0.0;  %arc_lenght of the slab 
+%3) Extract the variable concerning the center of circumference 
+C     = Terranes.Trench_properties.C;   %center of circumference
+R     = Terranes.Trench_properties.r;%Radius
+%4) Extract the properties of the bending angle 
+theta = Terranes.Trench_properties.theta*pi/180;% theta bending
+%5) Find the midplane radius 
+r    = sum(R)./length(R);% radius of mid plane 
+%6) Compute the angle 
+angle = 0.0*d; % angle layout
+D     = angle; % buffer vector containing the distance w.r.t. to the radius of curvature
+D(slab==1) = sqrt((x(slab==1)-C(1)).^2+(z(slab==1)- C(2)).^2);
+angle(slab==1)=acos((z(slab==1)-C(2))./D(slab==1)); % angle 
+% Empty the garbage 
+D      = []; 
+%7) Compute the length along the curved area 
+l(slab==1 & angle<=theta)=r.*angle(slab==1 & angle<=theta); 
+%8) Compute the length along the linear part. 
+% Coordinate of the point of midsurface after curvature
+PA(1)=C(1)+r*sin(theta);
+PA(2)=C(2)+r*cos(theta); 
+%Coordinate of the midsurface at the depth of slab 
+PB(1)=PA(1)+Terranes.Trench_properties.L0*cos(theta);
+PB(2)=PA(2)-Terranes.Trench_properties.L0*sin(theta); 
+% Compute the slope of the line: 
+m=(PB(2)-PA(2))./(PB(1)-PA(1)); 
+%m = 1./m;
+% project all the x points into the line (from:
+% https://math.stackexchange.com/questions/62633/orthogonal-projection-of-a-point-onto-a-line)
+% 
+xprojection = angle.*0.0; 
+zprojection = angle.*0.0;
+b = PA(2)-PA(1).*m;
+xprojection(slab==1 & l==0.0) = (x(slab==1 & l == 0.0) + m.*z(slab==1 & l==0.0)-m.*b)./(1+m.^2); 
+zprojection(slab==1 & l==0.0) = (m.*x(slab==1 & l == 0.0) + m^2.*z(slab==1 & l==0.0)+b)./(1+m.^2);
+l(slab==1 & l==0.0) = r*theta+sqrt((xprojection(slab==1 & l==0.0)-PA(1)).^2+(zprojection(slab==1 & l==0.0)-PA(2)).^2);
+
+LL = max(l(slab==1)); 
+% make dimensionless d and l; 
+l = l./Terranes.Trench_properties.D0; 
+d = d./Terranes.Trench_properties.D0;
+%======== Compute the temperature profile
+n = 50; 
+t_prov = Temp(:).*0.0; 
+Sigma    = t_prov; 
+Sigma(slab==1) = 0.0; 
+% Dividi et impera
+dn = 1-abs(d(slab==1));
+for i=1:n
+    a = (-1).^(i)./(i.*pi);
+    b = (Re-(Re^2+i^2*pi^2).^(0.5)).*l(slab==1);
+    c = sin(i.*pi.*(1-abs(d(slab==1))));
+    e = exp(b);
+    Sigma(slab==1) = Sigma(slab==1)+ a.*e.*c;  
+end
+    t_prov(slab==1) = (T_P)+2.*(T_P-T_S).*Sigma(slab==1); 
+    t_prov(slab==1 & t_prov<0) = T_S; 
+    weight = abs(z./Terranes.Trench_properties.D0);
+    weight(weight>1) = 0.9; 
+    Temp(slab==1)=(Temp(slab==1).*(1-weight(slab==1))+t_prov(slab==1)).*weight(slab==1); 
+    Temp = reshape(Temp,size(A.Xpart)); 
 end
 
 
@@ -347,8 +441,6 @@ if ~strcmp(type,'Mode_1')
     v = u;
     u(1,ind==1) = x(ind==1)';
     u(2,ind==1) = z(ind==1)';
-    v(1,ind==1) = Slab.C(1);
-    v(2,ind==1) = z(ind==1);
     d(ind==1) = sqrt((u(1,ind==1)-Slab.C(1)).^2+(u(2,ind==1)- Slab.C(2)).^2);
     arc_angleS(ind==1) = acos((z(ind==1)-Slab.C(2))./d(ind==1)).*180/pi;
     c_slab = d(ind==1)>=r(1) & d(ind==1)<=(r(2)) & arc_angleS(ind==1)<=theta & arc_angleS(ind==1)>=0;
@@ -612,7 +704,12 @@ cont = [];
 
 [Layout,~,cont] = find_slab_(A,Terranes.Trench_properties,'Slab');
 % Set the temperature
-[Temp] = compute_temperature_profile(Layout,Temp,-1,Gen,Terranes,ind_x);%(A,Temp,Type,T_tk,Gen,Terranes,indx)
+if strcmp(Terranes.Trench_properties.Temperature,'McKenzie')
+    [Temp] = compute_temperature_profile(Layout,Temp,-1,Gen,Terranes,ind_x);%(A,Temp,Type,T_tk,Gen,Terranes,indx
+    [Temp] = compute_temperature_profile_McKenzie(Layout,Temp,-1,Gen,Terranes,ind_x,A);%(A,Temp,Type,T_tk,Gen,Terranes,indx)
+else
+    [Temp] = compute_temperature_profile(Layout,Temp,-1,Gen,Terranes,ind_x);%(A,Temp,Type,T_tk,Gen,Terranes,indx)
+end
 % Correct Temperaure and Phase
 id1 = min(A.Xpart(~isnan(Layout)));
 id2 = max(A.Xpart(~isnan(Layout)));
